5add59cdfc44673c41da126f9edcc2a3d523f55f
Orienfish <x1yu@ucsd.edu>
2019-06-18 00:18:27 -0700

    frequency settings

---
+++ reset_freq.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Reset the frequency governor to ondemand
+# Usage: bash ./reset_freq.sh
+# set ondemand governor
+echo "ondemand" > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
+
---
+++ set_freq.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Set the frequency
+# Usage: to set the frequency to 600MHz: sudo bash ./set_freq.sh 600000
+freq=$1
+
+# set userspace governor
+echo "userspace" > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
+
+# set frequency
+echo $freq > /sys/devices/system/cpu/cpufreq/policy0/scaling_setspeed
