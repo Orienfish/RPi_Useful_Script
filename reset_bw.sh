826f7d3e2b4f3441be6e3c95807c2d9ede5c2d39
Orienfish <x1yu@ucsd.edu>
2019-06-18 00:23:55 -0700

    bandwidth settings

---
+++ reset_bw.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Clear bw setting using the wondershaper tool
+# Usage: sudo bash reset_bw.sh
+/home/pi/wondershaper/wondershaper -c -a wlan0
---
+++ set_bw.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Set bw using the wondershaper tool
+# Usage: to set a 100kbps bandwidth on both up and down link:
+# sudo bash set_bw.sh 100 
+bw=$1
+
+/home/pi/wondershaper/wondershaper -c -a wlan0
+/home/pi/wondershaper/wondershaper -a wlan0 -u $bw -d $bw
